7aa1c7670e999917ad84176699d4dc06338a5d54
HenryTurbedsky <turbo.henryg@gmail.com>
2021-08-04 17:04:59 +0000

    added tab menu

--- public/signIn.js
+++ public/signIn.js
@@ -10,7 +10,7 @@ const signIn = () => {
 
     // The signed-in user info.
     var user = result.user;
-    window.location = 'addInfo.html';
+    window.location = 'activities.html';
   }).catch((error) => {
     // Handle Errors here.
     var errorCode = error.code;
